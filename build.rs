36a6e65a1447b784b9ce9c09e992dd6180e314de
Ahmad Kemsan <ahmadkemsan@gmail.com>
2023-06-26 15:02:05 +0530

    test: check the dylib pathr

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
+use std::env;
+
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
     println!("cargo:rustc-link-search=.");
     println!("cargo:rustc-link-lib=dylib=LexActivator");
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", out_dir);
 
 }
