86c6cfcc969bae409e85dd54218bbd3d12514973
Ahmad Kemsan <ahmadkemsan@gmail.com>
2023-06-26 15:38:52 +0530

    test: static build

--- build.rs
+++ build.rs
@@ -1,10 +1,27 @@
-use std::env;
+// fn main() {
+//     println!("cargo:rustc-link-search=.");
+//     println!("cargo:rustc-link-lib=dylib=LexActivator");
+// }
 
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    println!("cargo:rustc-link-search=.");
-    println!("cargo:rustc-link-lib=dylib=LexActivator");
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", out_dir);
+    // Set the path to the directory containing the static library
+    let library_dir = ".";
 
+    // Specify the library name (without the lib prefix or file extension)
+    let library_name = "LexActivator";
+
+    // Set the target directory where the compiled binary will be placed
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let target_dir = out_dir.join("target");
+    let _ = std::fs::create_dir_all(&target_dir);
+
+    // Link against the static library
+    println!("cargo:rustc-link-search=native={}", library_dir);
+    println!("cargo:rustc-link-lib=static={}", library_name);
+
+    // Set the target directory as the place to put the resulting binary
+    println!("cargo:rustc-link-search={}", target_dir.display());
 }
