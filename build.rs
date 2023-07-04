dca4319aa204a964eb84dfab67e0eb82f736603a
Ahmad Kemsan <ahmadkemsan@gmail.com>
2023-07-04 16:02:25 +0530

    removed build rs lines

--- build.rs
+++ build.rs
@@ -6,8 +6,8 @@ fn main() {
     // Set the rpath flag to embed the library's location
     // let out_dir = env::var("OUT_DIR").unwrap();
     // println!("cargo:rustc-link-search={}/native={}", out_dir, library_path);
-    println!("cargo:rustc-link-search=native=.");
-    println!("cargo:rustc-link-lib=dylib=LexActivator");
+    // println!("cargo:rustc-link-search=native=.");
+    // println!("cargo:rustc-link-lib=dylib=LexActivator");
 
     // println!("cargo:rustc-link-lib=framework=CoreFoundation");
     // println!("cargo:rustc-link-lib=framework=SystemConfiguration");
